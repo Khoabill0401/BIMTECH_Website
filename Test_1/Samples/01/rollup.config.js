1f912187aa420d330252102a0b7bc293129e1de7
Khoabill0401 <khoabill0401@gmail.com>
2022-08-18 15:53:29 +0700

    Khoa_2nd_Commit

--- Test_1/Samples/01/rollup.config.js
+++ Test_1/Samples/01/rollup.config.js
@@ -1,12 +1,15 @@
 import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-node-resolve";
 
 export default {
   input: "Test_1\Samples\01\app.js",
   output: [
     {
+      // format: "iife",
       format: "esm",
       file: "Test_1\Samples\01\bundle.js",
     },
   ],
   plugins: [resolve()],
+  mode: 'development'
 };
