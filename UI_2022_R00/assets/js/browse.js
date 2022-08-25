e05ed95a96c6bea75fa615416b180201e1d197ba
Khoabill0401 <khoabill0401@gmail.com>
2022-08-25 15:06:37 +0700

    Khoa_14th_Commit

---
+++ UI_2022_R00/assets/js/browse.js
@@ -0,0 +1,13 @@
+/* ..............................................
+	   Gallery
+	   ................................................. */
+
+       $('#slides-shop').superslides({
+		inherit_width_from: '.cover-slides',
+		inherit_height_from: '.cover-slides',
+		play: 5000,
+		animation: 'fade',
+	});
+
+	$(".cover-slides ul li").append("<div class='overlay-background'></div>");
+
