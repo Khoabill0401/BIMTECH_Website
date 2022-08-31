49e7eda76daf23671ea5608ea91890c3ecb0a450
Khoabill0401 <khoabill0401@gmail.com>
2022-08-31 16:56:27 +0700

    Khoa_20th_Commit

--- SOCKET/rooms/app.js
+++ SOCKET/rooms/app.js
@@ -11,6 +11,7 @@ app.get('/',function(req, res) {
 
 io.on('connection', function(socket){
   console.log('a user connected ');
+  
   io.emit('rooms', getRooms('connected'));
   socket.on('disconnect', function(){
     console.log('user disconnected');
