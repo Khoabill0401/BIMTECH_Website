0a8751c5a1006b5fd2b6626a4f0123f037e0b974
Khoabill0401 <khoabill0401@gmail.com>
2022-11-10 01:07:12 +0700

    Update ProtectedRoutesComponent.js

--- MERN/frontend/src/components/ProtectedRoutesComponent.js
+++ MERN/frontend/src/components/ProtectedRoutesComponent.js
@@ -3,11 +3,11 @@ import { Outlet, Navigate} from "react-router-dom"
 const ProtectedRoutesComponent = ({admin}) => {
     let auth = false;
     if (admin) {
-        let adminAuth = false
-        if (adminAuth) auth = true
+        let adminAuth = false;
+        if (adminAuth) auth = true;
     } else {
-        let userAuth = true
-        if (userAuth) auth = true
+        let userAuth = true;
+        if (userAuth) auth = true;
     }
     return auth ? <Outlet /> : <Navigate to="/login" />;
 };
